72c346d87c592c586f9d64614d9151fe67a2c26e
utsaroyc <jayantiroyc@gmail.com>
2023-07-29 03:17:16 +0530

    final

---
+++ actions/getActiveProductsWithPrices.ts
@@ -0,0 +1,25 @@
+import { ProductWithPrice } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getProductsWithPrices = async (): Promise<ProductWithPrice[]> => {
+    const supabase = createServerComponentClient({
+        cookies: cookies,
+    });
+
+    const { data, error } = await supabase
+    .from('products')
+    .select('*, prices(*)')
+    .eq('active', true)
+    .eq('prices.active', true)
+    .order('metadata->index')
+    .order('unit_amount',{ foreignTable: 'prices' });
+
+    if (error){
+        console.error(error);
+    }
+
+    return (data as any) || [];
+};
+
+export default getProductsWithPrices;
